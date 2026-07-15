ca9d47ef83238bc01dd2c218fb57f57d4705b18d
abwfaat <reserveproject@mail.ru>
2026-07-15 16:50:21 +0300

    Initial commit

---
+++ src/test/java/com/awbp/mediadownloader/MediaDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awbp.mediadownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaDownloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
